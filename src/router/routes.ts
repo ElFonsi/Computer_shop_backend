82ebdc60d944659f674825a10873b4e416ffdf1e
Magallanes Alfonso <a.magallanes@etec.local>
2023-10-25 12:57:19 -0300

    m

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,9 +4,25 @@ const inicial = express.Router()
 inicial.get('/', (_, res) => {
     res.send('The sedulous hyena ate the antelope!');
   });
+  
+  const lista=[
+    {nombre:"PLaca Madre",
+    modelo:"B500",
+    precio:110,
+    pais_de_origen:"China"
+  },
 
+  {nombre:"Placa de video",
+  modelo:"1660",
+  precio:1000,
+  pais_de_origen:"China"
+}
+
+];  
 inicial.get('/home', (_, res) => {
-    res.send('Casa');
+  const preciosaltos = lista.filter((lista) => lista.precio > 100);
+
+    res.send(preciosaltos);
   });
 
 
